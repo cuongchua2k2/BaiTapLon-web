af7c0d41b0ae19d46c4254c7d8cc714203d42404
cuongchua2k2 <nguyenvancuong0345723186@gmail.com>
2024-08-25 21:33:01 +0700

    test

---
+++ admin/cartegoryadd.php
@@ -0,0 +1,12 @@
+<div class="admin-content__right">
+        <div class="admin-content__right__category-add">
+          <h1>Thêm Danh Mục</h1>
+          <form action="" method="POST">
+            <input type="text" placeholder="Nhập tên Danh mục" />
+            <button type="submit">Thêm</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
